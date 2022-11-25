a5efcdb57e80d19defef35e088a81727242b445b
DarcioCarvalho <darcio.nuno@gmail.com>
2022-11-25 13:29:30 -0300

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import { Router } from './Router';
 import './styles/global.css';
 
 export default function App() {
+  const basename = import.meta.env.DEV ? '' : '/duolar-estoque';
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <SearchProvider>
         <Router />
       </SearchProvider>
